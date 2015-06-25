d5664c9cb8157593eeef73355198f4a7fc533e76
Angelo Mario Del Grosso <angelo.delgrosso@ilc.cnr.it>
2015-06-25 11:18:00 +0200

    Test Omenga

---
+++ Omega/src/main/java/it/ilc/cnr/omega/WorkingTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.ilc.cnr.omega;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Angelo Del Grosso
+ */
+public class WorkingTest {
+
+    private static final Logger LOG = Logger.getLogger(WorkingTest.class.getName());
+
+    public static void main(String[] args) {
+        
+        LOG.log(Level.INFO, "start...");
+
+    }
+
+}
